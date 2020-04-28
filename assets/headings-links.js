bc9eb173ca6a267156d3d13fa1144359cba01f15
Gregwar <g.passault@gmail.com>
2020-04-28 17:34:08 +0200

    Anchor links on headings

---
+++ assets/headings-links.js
@@ -0,0 +1,11 @@
+// Thanks to
+// https://david.darn.es/2017/07/25/adding-heading-links-to-your-jekyll-blog/
+const headings = document.querySelectorAll('.page-content h1,h2,h3');
+const linkContent = '&nbsp;🔗';
+for (const heading of headings) {
+    const linkIcon = document.createElement('a');
+    linkIcon.setAttribute('class', "heading-link");
+    linkIcon.setAttribute('href', `#${heading.id}`);
+    linkIcon.innerHTML = linkContent;
+    heading.appendChild(linkIcon);
+}
